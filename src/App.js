4f8d8f39d9be0c1de955b532fae02485b6faff15
Denis Ouma <dennisouma38@gmail.com>
2023-03-10 20:57:20 +0300

    User able to edit a post and save to db

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./index.css";
 import BookCreate from "./components/BookCreate";
@@ -7,16 +7,25 @@ import BookList from "./components/BookList";
 function App() {
 	const [books, setBooks] = useState([]);
 
+	useEffect(() => {
+		getBooks();
+	}, []);
+
+	const getBooks = async () => {
+		const res = await axios.get("http://localhost:5000/books");
+
+		setBooks([...books, ...res.data]);
+		console.log(res.data);
+	};
 	// handle add a new Book
 	const createBook = async (title) => {
 		const res = await axios.post("http://localhost:5000/books", {
 			title,
 		});
 		setBooks([...books, res.data]);
-		console.log(res);
 	};
 
-	console.log(books);
+	console.log("Here are the Books", books);
 
 	// Handle Delete an Element
 	const deleteBookByid = (id) => {
@@ -26,14 +35,12 @@ function App() {
 
 	// Handle update a book
 
-	const editBookById = (id, title) => {
-		const editBook = books.map((book) => {
-			if (book.id === id) {
-				return { ...book, title };
-			}
-			return book;
+	const editBookById = async (id, title) => {
+		const res = await axios.put(`http://localhost:5000/books/${id}`, {
+			title,
 		});
-		setBooks(editBook);
+
+		setBooks([...books, res.data]);
 	};
 	return (
 		<div className="app">
